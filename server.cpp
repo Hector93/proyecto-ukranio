19640825333d393bfcc80e6795b9d408942567f7
VictorBoom <emarqui11@gmail.com>
2018-06-19 11:15:20 -0500

    Base de datos
    Se agrego la parte para crear la base de datos.

--- server.cpp
+++ server.cpp
@@ -20,6 +20,24 @@ int main() {
         cout << msg.arguments << endl;
         //res.sendReply(msg.arguments, msg.IP, msg.puerto);
 
+        if(!archivo.is_open()){
+                archivo.open("basededatos.txt", ios::app);
+            }
+            cout << "Telefono";
+            getline(cin, voto.numero);
+            cout << "RFC";
+            getline(cin, voto.RFC);
+            cout << "Partido";
+            getline(cin, voto.partido);
+
+            archivo << telefono << "," << rfc << "," << voto << endl;
+
+            system("cls");
+
+            cout << "Registro guardado";
+
+            archivo.close();
+
 
     }
     
