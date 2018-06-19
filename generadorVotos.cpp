86695d6471259706751d81fa68b4823ea5153c41
Hector93 <hmanuelg93@gmail.com>
2018-06-19 09:06:16 -0500

    creo estructura voto

---
+++ generadorVotos.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+const unsigned int numVotos = 88700000;
+
+int main(){
+  for(unsigned int i = 0; i < numVotos; i++){
+    
+  }
+  puts("termino");
+  return 0;
+}
---
+++ voto.h
@@ -0,0 +1,7 @@
+
+
+typedef struct{
+  char numero[10];
+  char RFC[13];
+  char partido;
+}voto;
